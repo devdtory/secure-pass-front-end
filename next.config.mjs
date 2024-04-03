d475ef08555c1720a882268784ddcd0ed0f0251f
Aryasah <aryasah30@gmail.com>
2024-04-03 16:40:37 +0530

    Update next.config.mjs with image domain and TypeScript configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true,
-    images: {
-        domains: ['images.unsplash.com'],
-    },
+  // reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
-    
+  },
 };
 
 export default nextConfig;
