d8af7f170902ef799b8d0b3d5ac9c245f939d062
Aryasah <aryasah30@gmail.com>
2024-04-11 20:43:56 +0530

    Update tracking value in Accordion component

--- src/components/shared/Accordion.tsx
+++ src/components/shared/Accordion.tsx
@@ -23,7 +23,7 @@ export const Disclosure = (props: Props) => {
       >
         <div className="flex justify-start items-center py-1">
           <div
-            className={`text-[14px] font-extrabold text-[#666666] uppercase font-generalSans tracking-[10%]`}
+            className={`text-[14px] font-extrabold text-[#666666] uppercase font-generalSans tracking-[0.075rem]`}
           >
             {props.title}
           </div>
